91a121cfe5e31af73249ed32a6f8875d035c5837
sahal-shinee <sahal00nurjamil@gmail.com>
2026-06-15 08:52:03 +0700

    fix the image verifikasi

--- resources/views/admin/verifikasi/index.blade.php
+++ resources/views/admin/verifikasi/index.blade.php
@@ -117,7 +117,7 @@
                                                  alt="KTP {{ $penyedia->nama_lengkap }}"
                                                  class="max-w-full h-auto max-h-[60vh] object-contain rounded-lg"
                                                  x-show="!broken"
-                                                 @error="broken = true">
+                                                 x-on:error="broken = true">
                                             <div x-show="broken" x-cloak class="flex flex-col items-center gap-3 py-8 text-gray-400">
                                                 <svg class="w-12 h-12" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>
